8c8ce1a5e7b4b3a54662fbc49111a5a3491a927c
Colin McKeon <14mckeon@gmail.com>
2016-11-06 21:04:54 -0500

    working on js to display adjustrecipe

--- app/scripts/components/adjustrecipe.jsx
+++ app/scripts/components/adjustrecipe.jsx
@@ -5,7 +5,38 @@ var Backbone = require('backbone');
 var AdjustRecipe = React.createClass({
     render: function(){
         return(
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3">
+                <div className="recipe-container">
 
+                    <div className="adjustrecipe-header">
+                        <div className="servings">
+                            <span>Makes</span>
+                            <input id="servings-input" name="servings-input" placeholder="Qty">
+                            <span>servings.</span>
+                        </div>
+                        <div className="radio-button-container">
+                            <input type="radio" value="US" checked> US
+                            <input type="radio" value="US"> Metric
+                        </div>
+                        <div className="adjustrecipe-button-container">
+                            <input className="btn btn-info" type="submit" value="Adjust Recipe">
+                        </div>
+                    </div>
+                    <div className="recipe-items">
+                          <label><input type="checkbox"> (recipe item)</label>
+                          <br>
+                    </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
         )
     }
 });
+
+module.exports = {
+  AdjustRecipe: AdjustRecipe
+}
