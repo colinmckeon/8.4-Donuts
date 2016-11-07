f18810797b460887d1e03ac4d8aa985670fd22fe
Colin McKeon <14mckeon@gmail.com>
2016-11-06 22:08:21 -0500

    working on form

---
+++ app/scripts/components/recipeform.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var Backbone = require('backbone');
+
+var RecipeForm = React.createClass({
+    render: function(){
+        return (
+
+        )
+    }
+});
+
+var RecipeFormContainer = React.createClass({
+    render: function(){
+        return (
+          
+        )
+    }
+})
+
+module.exports = {
+
+}
