42ebc579d515b8ea5974fb56305cb7313d91f750
Colin McKeon <14mckeon@gmail.com>
2016-11-06 19:36:27 -0500

    recipe data

---
+++ app/scripts/data/data.js
@@ -0,0 +1,45 @@
+var recipeItems = [{
+    id: '01',
+    title: 'Avocado',
+    measurement-type: 'ripe',
+    quantity: 2
+  }, {
+    id: '02',
+    title: 'Kosher salt',
+    measurement-type: 'teaspoon',
+    quantity: .5
+  }, {
+    id: '03',
+    title: 'Lime juice',
+    measurement-type: 'Tbsp',
+    quantity: 1
+  }, {
+    id: '04',
+    title: 'minced red onion',
+    measurement-type: 'cup',
+    quantity: .25
+  }, {
+    id: '05',
+    title: 'serrano chiles',
+    measurement-type: '',
+    quantity: 2
+  }, {
+    id: '06',
+    title: 'cilantro',
+    measurement-type: 'Tbsp',
+    quantity: 2
+  }, {
+    id: '07',
+    title: 'black pepper',
+    measurement-type: 'dash',
+    quantity:
+  }, {
+    id: '08',
+    title: 'tomato',
+    measurement-type: 'ripe',
+    quantity: .5
+}];
+
+module.exports = {
+  recipeItems: recipeItems
+};
